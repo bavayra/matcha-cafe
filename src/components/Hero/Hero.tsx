b67c1eb5978cf8a6b1086756c9f6fba27a7faa4e
Alix Bell <x.bavayra@gmail.com>
2026-07-23 19:37:54 +0200

    feat/refactor/style(Hero): MAJOR REMAKE: added new bg imgs, set them differently for mobile and desktop screens. Removed video file from the project. Removed border. Removed padding-top for the text content. For CTA btn: set bg to blur instead of color, created and used css class for its font size, for 260+ screens increased font size, decreased padding-x and set font weight to light. For the paragraph, increased font size. For md screens, removed padding-tp[ and text align to left from the h1 container, decreased margin-top for the CTA btn. For lg screens, removed grid layout a alignment to left, centered the content and increased h1's font size. For 2xl screens, removed margin-right for the h1. For 1920+ screens, increased font size for the h1 (created and used css class). For 2460+ screens, removed h1-margin css class. Removed redundant code

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,46 +2,31 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="relative h-svh min-h-[580px] flex flex-col justify-center px-6 md:px-12 lg:px-20 z-10 overflow-hidden border-b-4 border-[var(--cream-main)]"
-      style={{
-        backgroundColor: 'color-mix(in srgb, var(--color-classic-section-bg) 60%, transparent)',
-      }}
+      className="relative h-svh min-h-[580px] flex flex-col justify-center px-6 md:px-12 lg:px-20 z-10 overflow-hidden bg-[url('/images/photos/bg-mobil.webp')] lg:bg-[url('/images/photos/bg-desktop.webp')] bg-no-repeat opacity-90 bg-center bg-cover"
     >
-      <div className="lg:grid lg:grid-cols-2 lg:gap-24 lg:max-w-7xl lg:mx-auto">
-        {/*
-        <video
-          autoPlay
-          muted
-          playsInline
-          className="hidden lg:flex absolute top-0 right-0 max-w-[30vw] h-auto object-contain pointer-events-none opacity-90"
-        >
-          <source src="images/drinks/matcha-vid.mp4" type="video/mp4" />
-        </video> */}
-
-        <div className="relative z-10 flex-1 flex flex-col items-center lg:items-start justify-center text-center md:text-left w-full max-w-7xl pt-16 md:pt-0">
-          <div className="flex flex-col 2xl:mr-10 items-center max-w-xl h1-margin">
-            <h1 className="text-[clamp(72px,calc(11.56462585vw+30.367347px),208px)] font-bold text-[var(--cream-main)] leading-none tracking-tight">
-              SULU
-            </h1>
-            <p className="text-[clamp(18px,calc(1.7741935vw+11.6139034px),40px)] text-[var(--cream-main)] font-light leading-relaxed">
-              та самая матча
-            </p>
-            <a
-              href="#drinks"
-              className="mt-4 md:mt-10 md:text-2xl inline-flex items-center gap-2 px-8 py-3 bg-[var(--green-main)] text-[var(--yellow-text)] rounded-xl hover:bg-[var(--yellow-main)] hover:text-[var(--green-main)] hover:scale-110 active:scale-95 hover-transition"
-            >
-              выбирай матчу
-              <svg viewBox="0 0 24 24" fill="none" aria-hidden className="w-4 h-4 md:w-6 md:h-6">
-                <path
-                  d="M5 12h14M12 5l7 7-7 7"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </a>
-          </div>
+      <div className="relative z-10 flex-1 flex flex-col items-center justify-center text-center w-full max-w-7xl mx-auto">
+        <div className="flex flex-col items-center max-w-xl h1-margin">
+          <h1 className="text-[7.5rem] lg:text-[12rem] h1-font-size font-bold text-[var(--cream-main)] leading-none tracking-tight">
+            SULU
+          </h1>
+          <p className="text-2xl lg:text-4xl text-[var(--cream-main)] font-light leading-relaxed">
+            та самая матча
+          </p>
+          <a
+            href="#drinks"
+            className="mt-4 md:mt-6 cta-font-size inline-flex items-center gap-2 px-6 py-3 text-[var(--yellow-main)] rounded-xl hover:bg-[var(--yellow-main)] hover:text-[var(--green-main)] hover:scale-110 active:scale-95 hover-transition backdrop-blur-md shadow-xl font-light bg-transparent"
+          >
+            выбирай матчу
+            <svg viewBox="0 0 24 24" fill="none" aria-hidden className="w-4 h-4 md:w-6 md:h-6">
+              <path
+                d="M5 12h14M12 5l7 7-7 7"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </a>
         </div>
       </div>
 
